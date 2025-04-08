fa1c6e1bae88acb19cd40d233dd0843590309e38
braguilera <braaguileraa@gmail.com>
2025-04-08 16:51:40 -0300

    Screens de Cursos y recetas

---
+++ screens/courses/HomeCourses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeCourses = () => {
+  return (
+    <View className='w-full h-full flex justify-center items-center'>
+        <Text>Cursos</Text>
+    </View>
+      
+  )
+}
+
+export default HomeCourses
---
+++ screens/recipes/HomeRecipes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeRecipes = () => {
+  return (
+    <View className='w-full h-full flex justify-center items-center'>
+        <Text>Rcetas</Text>
+    </View>
+  )
+}
+
+export default HomeRecipes
